02be1b9f49edc4c2e5cefcc82e958bcbefa4c9a5
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 15:13:12 +0530

    feat: javascript basic task2

---
+++ 01. Basic/JS_Task_2.js
@@ -0,0 +1,15 @@
+// Write a JavaScript program to find the area of a triangle where 
+// three sides are 5, 6, 7. 
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+const base = 5;
+const height = 6;
+const width = 7;
+// area of triangle = (a+b+c)/2
+let area = (base + height + width )/2;
+alert(area);
+
+
+/* --------------------------------------------------------------------------------- */
