b4ce25eda31cf3bcaf864f556fcc0011d5be8776
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 18:11:15 +0530

    feat: javascript variables task6

---
+++ 02. Variables/JS_Task_6.js
@@ -0,0 +1,17 @@
+// A visitor visits an online clothing store www.xyzClothing.com. 
+// Write a script to store in variables the following information:
+    // a. Visitor’s name
+    // b. Product title
+    // c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: 
+// “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+let Visitors_name = "Jananni";
+let Product_title = "T-shirt";
+let Quantity = 2;
+console.log(`${Visitors_name} ordered ${Quantity} ${Product_title}(s) on XYZ Clothing `);
+
+/* --------------------------------------------------------------------------------- */
