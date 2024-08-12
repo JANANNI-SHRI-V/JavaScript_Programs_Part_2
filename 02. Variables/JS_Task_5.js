447b06adddd5da9caa10436e99cecbc19247d4e3
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 17:55:51 +0530

    feat: javascript variables task5

---
+++ 02. Variables/JS_Task_5.js
@@ -0,0 +1,9 @@
+// Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+let age;
+age = 20;
+alert(age);
+/* --------------------------------------------------------------------------------- */
