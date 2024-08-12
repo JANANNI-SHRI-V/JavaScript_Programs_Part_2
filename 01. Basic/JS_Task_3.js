a322f975ea7a87b6cc13981a6e8e4ba3a1d98c0c
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 15:26:37 +0530

    feat: javascript basic task3

---
+++ 01. Basic/JS_Task_3.js
@@ -0,0 +1,17 @@
+// Write a JavaScript program to compute the sum of the two given integers. 
+// If the two values are the same, then return triple their sum.  
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+let a = 5
+let b = 5
+let sum = a + b;
+if (a == b) {
+    console.log((a + b) * 3);
+}else{
+    console.log(sum);
+}
+
+
+/* --------------------------------------------------------------------------------- */
