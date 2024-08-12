ebbed0604fd4cd2b32e6c1133d3917d49abc8d32
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 17:52:05 +0530

    feat: javascript variables task4

---
+++ 02. Variables/JS_Task_4.js
@@ -0,0 +1,20 @@
+// Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+// declaring variables to store student's bio data 
+let Name = "Amutha";
+let Age = 20;
+let Address = "Coimbatore";
+let Phone = 9876536200;
+let Email = "amutha@gmail.com";
+
+// dispalying the message in alert boxes
+alert("Name: "+ Name);
+alert("Age: "+ Age);
+alert("Address: "+ Address);
+alert("Phone: "+ Phone);
+alert("Email: "+ Email);
+
+/* --------------------------------------------------------------------------------- */
