d13487b46c9bf05ddb88de6ca97bc520a5269878
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 17:38:48 +0530

    feat: javascript variables task3

---
+++ 02. Variables/JS_Task_3.js
@@ -0,0 +1,12 @@
+// Write script to:
+    // a) Declare a JS variable, titled message
+    // b) Assign “Hello World” to variable message
+    // c) Display the message in alert box.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+let message;
+message = "Hello World";
+alert(message);
+/* --------------------------------------------------------------------------------- */
