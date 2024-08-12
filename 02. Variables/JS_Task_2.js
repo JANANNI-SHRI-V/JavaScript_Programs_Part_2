5fca669a29957f4b1494c1a40af14d94ad71b7ae
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-12 17:34:59 +0530

    feat: javascript variables task2

---
+++ 02. Variables/JS_Task_2.js
@@ -0,0 +1,12 @@
+// Declare a variable called myName & assign to it a string that represents your Full Name.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+let myName = "Jananni Shri";
+console.log(myName);
+/* --------------------------------------------------------------------------------- */
+
+
+
+
