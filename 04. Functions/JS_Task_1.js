d4051994f83ff579db8b8ac8028399960d574f1b
JANANNI-SHRI-V <2222ja20@kgcas.com>
2024-08-17 22:27:17 +0530

    feat: javascript functions task1

---
+++ 04. Functions/JS_Task_1.js
@@ -0,0 +1,19 @@
+// Write a function that takes first & last name and then it greets the user using his full name.
+
+/* --------------------------------------------------------------------------------- */
+
+/* YOUR CODE HERE */  
+
+let firstName = prompt("Enter your FirstName: ");
+let lastName = prompt("Enter your LastName: ");
+function greetUser(firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+    console.log("Hello, " + fullName + "!");
+    }
+    greetUser(firstName, lastName);
+
+
+/* --------------------------------------------------------------------------------- */
+
+
+
